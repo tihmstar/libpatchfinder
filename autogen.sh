994e0893190173faeaf8b04aade212ba5f9091ee
tihmstar <tihmstar@gmail.com>
2018-02-17 22:43:29 +0100

    i have no idea what i'm doing. stuff might work now, but it wouldn't surprise me if it doesn't

--- autogen.sh
+++ autogen.sh
@@ -10,4 +10,13 @@ aclocal -I m4
 autoconf
 autoheader
 automake --add-missing
-./configure "$@"
+
+
+SUBDIRS="external/libplist"
+for SUB in $SUBDIRS; do
+    pushd $SUB
+    CFLAGS="-arch arm64 -isysroot $(xcrun --show-sdk-path --sdk iphoneos)" CPPFLAGS="-arch arm64 -isysroot $(xcrun --show-sdk-path --sdk iphoneos)" ./autogen.sh --enable-static --disable-shared --host=arm-apple-darwin --without-cython
+    popd
+done
+
+CFLAGS="-arch arm64 -isysroot $(xcrun --show-sdk-path --sdk iphoneos)" CPPFLAGS="-arch arm64 -isysroot $(xcrun --show-sdk-path --sdk iphoneos)" ./configure --host=arm-apple-darwin --enable-static --disable-shared "$@"
