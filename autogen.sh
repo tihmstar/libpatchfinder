182d40d7765e1415d9a05ed8276c40f8d6964730
tihmstar <tihmstar@gmail.com>
2019-10-01 16:17:57 +0200

    fixed autogen for default host

--- autogen.sh
+++ autogen.sh
@@ -12,19 +12,16 @@ autoheader
 automake --add-missing
 
 
-SYSROOT="$(xcrun --show-sdk-path --sdk iphoneos)"
-export CFLAGS+="-arch arm64 -isysroot $SYSROOT -DIMG4TOOL_NOMAIN"
-export CXXFLAGS+="-arch arm64 -isysroot $SYSROOT -DIMG4TOOL_NOMAIN"
+# SYSROOT="$(xcrun --show-sdk-path --sdk iphoneos)"
+# export CFLAGS+="-arch arm64 -isysroot $SYSROOT -DIMG4TOOL_NOMAIN"
+# export CXXFLAGS+="-arch arm64 -isysroot $SYSROOT -DIMG4TOOL_NOMAIN"
 
 # otherwise img4tool cant find libplist
-CFLAGS+=" -I$PWD/external/libplist/include"
-CXXFLAGS+=" -I$PWD/external/libplist/include"
+CFLAGS+=" -DIMG4TOOL_NOMAIN -I$PWD/external/libplist/include"
+CXXFLAGS+=" -DIMG4TOOL_NOMAIN -I$PWD/external/libplist/include"
 export LDFLAGS+="-L$PWD/external/libplist/src"
 
-CONFIGURE_FLAGS="--enable-static --disable-shared\
-  --build=x86_64-apple-darwin`uname -r` \
-  --host=aarch64-apple-darwin \
-  --without-cython --without-openssl \
+CONFIGURE_FLAGS="--without-cython --without-openssl \
   --without-lzfse \
   $@"
 
@@ -36,4 +33,3 @@ for SUB in $SUBDIRS; do
 done
 
 ./configure $CONFIGURE_FLAGS
-
